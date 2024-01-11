79111ece0a697e7920e7a496b76d4363bd0862bf
David Carmichael <carmichaelits@gmail.com>
2024-01-11 12:43:54 +0000

    Message on Loading comp optional

--- app_vite/components/Loading.jsx
+++ app_vite/components/Loading.jsx
@@ -1,4 +1,4 @@
-export default function Loading() {
+export default function Loading(props) {
     return (
         <div className={"loading"}>
             <svg width="24" height="24" stroke="#000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -6,7 +6,7 @@ export default function Loading() {
                     <circle cx="12" cy="12" r="9.5" fill="none" stroke-width="3"></circle>
                 </g>
             </svg>
-            <p>Loading, please wait...</p>
+            {props.message ? <p>{props.message}</p> : ''}
         </div>
     )
 }
