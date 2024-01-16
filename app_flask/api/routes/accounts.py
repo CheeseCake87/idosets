f7fbc3d18846172ce186051dfd0a869b3202b47e
David Carmichael <carmichaelits@gmail.com>
2024-01-16 17:23:49 +0000

    feat(app-flask): check if url account is the same as session account

--- app_flask/api/routes/accounts.py
+++ app_flask/api/routes/accounts.py
@@ -1,3 +1,4 @@
+from flask import session
 from flask_imp.security import api_login_check
 
 from .. import bp
@@ -6,17 +7,23 @@ from app_flask.models.workouts import Workouts
 from app_flask.models.exercises import Exercises
 from app_flask.models.sets import Sets
 
+
 @bp.get("/accounts/<account_id>")
 @api_login_check(
     "logged_in", True, {"status": "unauthorized", "message": "unauthorized"}
 )
 def account_(account_id):
-    jsonr = {
-        "status": "success",
-        "message": "Account found.",
-        **Accounts.get_account_info(account_id),
-        "total_workouts": Workouts.count_by_account_id(account_id),
-        "total_exercises": Exercises.count_by_account_id(account_id),
-        "total_sets": Sets.count_by_account_id(account_id),
+    if account_id == session.get("account_id", 0):
+        return {
+            "status": "success",
+            "message": "Account found.",
+            **Accounts.get_account_info(account_id),
+            "total_workouts": Workouts.count_by_account_id(account_id),
+            "total_exercises": Exercises.count_by_account_id(account_id),
+            "total_sets": Sets.count_by_account_id(account_id),
+        }
+    return {
+        "status": "failed",
+        "message": "Account not found.",
+        "account_id": 0,
     }
-    return jsonr
