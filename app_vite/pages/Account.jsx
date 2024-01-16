e2e19ed3d6aeef7f05af33c2261d742278aaa297
David Carmichael <carmichaelits@gmail.com>
2024-01-16 17:46:56 +0000

    feat(app-vite): add account stats, and theme change button to Account page

--- app_vite/pages/Account.jsx
+++ app_vite/pages/Account.jsx
@@ -11,12 +11,64 @@ export default function Account() {
     const [ctx, setCtx] = useContext(mainContext);
     const navigate = useNavigate();
 
-    const account = new Fetcher({account_id: ctx.account_id}, ctx.getAccount)
+    const account = new Fetcher(
+        {account_id: ctx.account_id},
+        ctx.getAccount
+    )
 
     function Page() {
         return (
             <div className={"container"}>
+                <div className={"display-box flex-col gap-2 mb-2"}>
+                    <p className={'font-bold mb-2'}>Account</p>
+                    <p>
+                        <span className={'opacity-80'}>Email Address: </span>
+                        {account.get("email_address")}
+                    </p>
+                    <p>
+                        <span className={'opacity-80'}>Total Workouts: </span>
+                        {account.get("total_workouts")}
+                    </p>
+                    <p>
+                        <span className={'opacity-80'}>Total Exercises: </span>
+                        {account.get("total_exercises")}
+                    </p>
+                    <p>
+                        <span className={'opacity-80'}>Total Sets: </span>
+                        {account.get("total_sets")}
+                    </p>
+                </div>
 
+                <div className={"display-box flex-col gap-2"}>
+                    <p className={'font-bold mb-2'}>Change Theme</p>
+                    <div>
+                        <Show when={ctx.theme === 'dark'}>
+                            <button
+                                className={"flex rounded-full"}
+                                onClick={() => {
+                                    ctx.setTheme('light').then(json => {
+                                        setCtx('theme', json.theme)
+                                    })
+                                }}
+                            >
+                                <span className="material-icons-round">light_mode</span>
+                            </button>
+                        </Show>
+
+                        <Show when={ctx.theme === 'light'}>
+                            <button
+                                className={"flex rounded-full"}
+                                onClick={() => {
+                                    ctx.setTheme('dark').then(json => {
+                                        setCtx('theme', json.theme)
+                                    })
+                                }}
+                            >
+                                <span className="material-icons-round">dark_mode</span>
+                            </button>
+                        </Show>
+                    </div>
+                </div>
             </div>
         )
     }
@@ -28,3 +80,7 @@ export default function Account() {
         </>
     );
 };
+
+
+
+
