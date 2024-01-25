a50466d599cc2e1c309399dd8d59937ecee8acf6
David Carmichael <carmichaelits@gmail.com>
2024-01-25 21:14:59 +0000

    feat: split Workouts page into components

--- app_vite/pages/Workouts.jsx
+++ app_vite/pages/Workouts.jsx
@@ -38,176 +38,198 @@ export default function Workouts() {
         }
     })
 
+    function StartWorkoutPopOut(props) {
+        let [workout, i] = props
+        return (
+            <Show
+                when={startWorkout() === i() && activeSessions()[workout.workout_id] === undefined}>
+                <div className={"display-box flex-reactive items-center justify-between mt-4"}>
+                    <p>Are you sure you want to start this workout?</p>
+                    <div className={'flex gap-2'}>
+
+                        <button onClick={() => {
+                            setStartWorkout(null)
+                        }}>
+                            Not ready
+                        </button>
+                        <button className={'button-good'} onClick={() => {
+                            ctx.startSession(
+                                {workout_id: workout.workout_id}
+                            ).then(json => {
+                                if (json.status === 'success') {
+                                    navigate(
+                                        `/workout/${workout.workout_id}` +
+                                        `/session/${json.workout_session_id}`
+                                    )
+                                }
+                            })
+                        }}>
+                            Start
+                        </button>
+                    </div>
+                </div>
+            </Show>
+        )
+    }
+
+    function DeleteWorkoutPopOut(props) {
+        let [workout, i] = props
+        return (
+            <Show when={deleteWorkout() === i()}>
+                <div
+                    className={"display-box flex-reactive items-center justify-between mt-4"}>
+                    <p>Are you sure you want to delete this workout?</p>
+
+                    <div className={'flex gap-2'}>
+                        <button className={'button-bad'} onClick={() => {
+                            ctx.deleteWorkout({
+                                workout_id: workout.workout_id,
+                            }).then(json => {
+                                if (json.status === 'success') {
+                                    setDeleteWorkout(null)
+                                    workouts.refetch()
+                                }
+                            })
+                        }}>
+                            Delete
+                        </button>
+                        <button onClick={() => {
+                            setDeleteWorkout(null)
+                        }}>
+                            No
+                        </button>
+                    </div>
+
+                </div>
+            </Show>
+        )
+    }
+
+    function LoopWorkouts() {
+        return (
+            <For each={workouts.get("Workouts")}>
+                {(workout, i) =>
+                    <div className={"display-box flex-col"}>
+
+                        <div className={'flex-reactive justify-between'}>
+
+                            <div className={'flex flex-col pb-2'}>
+                                <h1 className={'m-0'}>{workout.name}</h1>
+                                <p>{workout.rel_exercises.length} Exercises</p>
+                            </div>
+
+                            <div className={'action-options items-center justify-between gap-2'}>
+                                <div className={"action"} onClick={() => {
+                                    setDeleteWorkout(i())
+                                }}>
+                                    <span className="material-icons">delete</span>
+                                </div>
+                                <div className={'flex gap-2'}>
+                                    <div className={"action"} onClick={() => {
+                                        navigate(`/workout/${workout.workout_id}`)
+                                    }}>
+                                        <span className="material-icons">edit</span>
+                                    </div>
+                                    <Show
+                                        when={activeSessions()[workout.workout_id] !== undefined}
+                                        fallback={
+                                            <div className={"action"} onClick={() => {
+                                                setStartWorkout(i())
+                                            }}>
+                                                <span className="material-icons">start</span>
+                                            </div>
+                                        }
+                                    >
+                                        <div className={"action flashing-good"} onClick={() => {
+                                            navigate(
+                                                `/workout/${workout.workout_id}/` +
+                                                `session/` +
+                                                `${activeSessions()[workout.workout_id].workout_session_id}`
+                                            )
+                                        }}>
+                                            <span className="material-icons">start</span>
+                                        </div>
+                                    < /Show>
+                                </div>
+                            </div>
+                        </div>
+                        <DeleteWorkoutPopOut workout={workout} i={i}/>
+                        <StartWorkoutPopOut workout={workout} i={i}/>
+                    </div>
+                }
+            </For>
+        )
+    }
+
+    function AddWorkout() {
+        return (
+            <Show when={addingWorkout() === true} fallback={
+                <div className={"action-box-clickable p-10"} onClick={() => {
+                    setAddingWorkout(true)
+                }}>
+                    <span className="material-icons px-2">add</span> Workout
+                </div>
+            }>
+                <div className={"action-box"}>
+
+                    <form className={"form-col"}
+                          onSubmit={(e) => {
+                              e.preventDefault()
+                          }}>
+                        <input
+                            className={"flex-1"}
+                            type="text"
+                            id="new_workout_name"
+                            name="new_workout_name"
+                            placeholder={"Workout Name"}
+                            onKeyUp={(e) => {
+                                setNewWorkoutName(e.target.value)
+                            }}
+                        />
+                        <div className={'flex justify-between gap-4 pt-2'}>
+
+                            <button
+                                className={"button-bad flex-1"}
+                                type="button"
+                                onClick={() => {
+                                    setAddingWorkout(false)
+                                    setNewWorkoutName('')
+                                }}>
+                                Cancel
+                            </button>
+
+                            <button
+                                className={"button-good flex-1"}
+                                type="button"
+                                onClick={() => {
+                                    ctx.addWorkout({
+                                        name: newWorkoutName(),
+                                    }).then(json => {
+                                        if (json.status === 'success') {
+                                            setAddingWorkout(false)
+                                            setNewWorkoutName('')
+                                            navigate(`/workout/${json["workout_id"]}`)
+                                        }
+                                    })
+                                }}>
+                                Add
+                            </button>
+
+                        </div>
+                    </form>
+
+                </div>
+            </Show>
+        )
+    }
+
     function Page() {
         return (
             <div className={"container"}>
                 <div className={"pb-4 flex flex-col gap-2"}>
-                    <For each={workouts.get("Workouts")}>
-                        {(workout, i) =>
-                            <div className={"display-box flex-col"}>
-
-                                <div className={'flex-reactive justify-between'}>
-
-                                    <div className={'flex flex-col pb-2'}>
-                                        <h1 className={'m-0'}>{workout.name}</h1>
-                                        <p>{workout.rel_exercises.length} Exercises</p>
-                                    </div>
-
-                                    <div className={'action-options items-center justify-between gap-2'}>
-
-                                        <div className={"action"} onClick={() => {
-                                            setDeleteWorkout(i())
-                                        }}>
-                                            <span className="material-icons">delete</span>
-                                        </div>
-                                        <div className={'flex gap-2'}>
-                                            <div className={"action"} onClick={() => {
-                                                navigate(`/workout/${workout.workout_id}`)
-                                            }}>
-                                                <span className="material-icons">edit</span>
-                                            </div>
-                                            <Show
-                                                when={activeSessions()[workout.workout_id] !== undefined}
-                                                fallback={
-                                                    <div className={"action"} onClick={() => {
-                                                        setStartWorkout(i())
-                                                    }}>
-                                                        <span className="material-icons">start</span>
-                                                    </div>
-                                                }
-                                            >
-                                                <div className={"action flashing-good"} onClick={() => {
-                                                    navigate(
-                                                        `/workout/${workout.workout_id}/` +
-                                                        `session/` +
-                                                        `${activeSessions()[workout.workout_id].workout_session_id}`
-                                                    )
-                                                }}>
-                                                    <span className="material-icons">start</span>
-                                                </div>
-                                            < /Show>
-                                        </div>
-                                    </div>
-
-                                </div>
-
-                                <Show when={deleteWorkout() === i()}>
-                                    <div
-                                        className={"display-box flex-reactive items-center justify-between mt-4"}>
-                                        <p>Are you sure you want to delete this workout?</p>
-
-                                        <div className={'flex gap-2'}>
-                                            <button className={'button-bad'} onClick={() => {
-                                                ctx.deleteWorkout({
-                                                    workout_id: workout.workout_id,
-                                                }).then(json => {
-                                                    if (json.status === 'success') {
-                                                        setDeleteWorkout(null)
-                                                        workouts.refetch()
-                                                    }
-                                                })
-                                            }}>
-                                                Delete
-                                            </button>
-                                            <button onClick={() => {
-                                                setDeleteWorkout(null)
-                                            }}>
-                                                No
-                                            </button>
-                                        </div>
-
-                                    </div>
-                                </Show>
-
-                                <Show
-                                    when={startWorkout() === i() && activeSessions()[workout.workout_id] === undefined}>
-                                    <div className={"display-box flex-reactive items-center justify-between mt-4"}>
-                                        <p>Are you sure you want to start this workout?</p>
-                                        <div className={'flex gap-2'}>
-
-                                            <button onClick={() => {
-                                                setStartWorkout(null)
-                                            }}>
-                                                Not ready
-                                            </button>
-                                            <button className={'button-good'} onClick={() => {
-                                                ctx.startSession(
-                                                    {workout_id: workout.workout_id}
-                                                ).then(json => {
-                                                    if (json.status === 'success') {
-                                                        navigate(`/workout/${workout.workout_id}/session/${json.workout_session_id}`)
-                                                    }
-                                                })
-                                            }}>
-                                                Start
-                                            </button>
-                                        </div>
-                                    </div>
-                                </Show>
-
-                            </div>
-                        }
-                    </For>
+                    <LoopWorkouts/>
                 </div>
-
-                <Show when={addingWorkout() === true} fallback={
-                    <div className={"action-box-clickable p-10"} onClick={() => {
-                        setAddingWorkout(true)
-                    }}>
-                        <span className="material-icons px-2">add</span> Workout
-                    </div>
-                }>
-                    <div className={"action-box"}>
-
-                        <form className={"form-col"}
-                              onSubmit={(e) => {
-                                  e.preventDefault()
-                              }}>
-                            <input
-                                className={"flex-1"}
-                                type="text"
-                                id="new_workout_name"
-                                name="new_workout_name"
-                                placeholder={"Workout Name"}
-                                onKeyUp={(e) => {
-                                    setNewWorkoutName(e.target.value)
-                                }}
-                            />
-                            <div className={'flex justify-between gap-4 pt-2'}>
-
-                                <button
-                                    className={"button-bad flex-1"}
-                                    type="button"
-                                    onClick={() => {
-                                        setAddingWorkout(false)
-                                        setNewWorkoutName('')
-                                    }}>
-                                    Cancel
-                                </button>
-
-                                <button
-                                    className={"button-good flex-1"}
-                                    type="button"
-                                    onClick={() => {
-                                        ctx.addWorkout({
-                                            name: newWorkoutName(),
-                                        }).then(json => {
-                                            if (json.status === 'success') {
-                                                setAddingWorkout(false)
-                                                setNewWorkoutName('')
-                                                navigate(`/workout/${json["workout_id"]}`)
-                                            }
-                                        })
-                                    }}>
-                                    Add
-                                </button>
-
-                            </div>
-                        </form>
-
-                    </div>
-                </Show>
-
+                <AddWorkout/>
             </div>
         )
     }
@@ -215,7 +237,11 @@ export default function Workouts() {
     return (
         <>
             <TopMenu/>
-            {workouts.data.loading ? <div className={"pt-10"}><Loading/></div> : <Page/>}
+            {
+                workouts.data.loading ?
+                    <div className={"pt-10"}><Loading/></div> :
+                    <Page/>
+            }
         </>
     );
 };
