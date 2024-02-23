12d4cf54aacc94b9695007a548bccf9ff522874a
David Carmichael <carmichaelits@gmail.com>
2024-02-23 08:25:21 +0000

    feat: add front page route

---
+++ app_vite/pages/Index.jsx
@@ -0,0 +1,10 @@
+export default function Index () {
+  return (
+      <div className={'container'}>
+          <div className={'text-center pt-2 pb-8'}>
+              <h1 className={'text-6xl mb-5'}>💪 I Do Sets</h1>
+              <p>A workout app focused on doing sets.</p>
+          </div>
+      </div>
+  )
+};
